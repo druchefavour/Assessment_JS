9dfa7e7b6083ee649b201eab9d9a01d380eb45ff
druchefavour <druchefavour@gmail.com>
2017-06-02 19:58:06 -0500

    commit async_test

---
+++ arrays.js
@@ -0,0 +1,66 @@
+exports = typeof window === 'undefined' ? global : window;
+
+exports.arraysAnswers = {
+  indexOf: function(arr, item) {
+    // loop through the entire array and return the number it is equivalent to the  position of the number in the aray
+
+    for (var i =0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    } 
+   return -1;
+  },
+
+  sum: function(arr) {
+
+  },
+
+  remove: function(arr, item) {
+
+  },
+
+  removeWithoutCopy: function(arr, item) {
+
+  },
+
+  append: function(arr, item) {
+
+  },
+
+  truncate: function(arr) {
+
+  },
+
+  prepend: function(arr, item) {
+
+  },
+
+  curtail: function(arr) {
+
+  },
+
+  concat: function(arr1, arr2) {
+
+  },
+
+  insert: function(arr, item, index) {
+
+  },
+
+  count: function(arr, item) {
+
+  },
+
+  duplicates: function(arr) {
+
+  },
+
+  square: function(arr) {
+
+  },
+
+  findAllOccurrences: function(arr, target) {
+
+  }
+};
