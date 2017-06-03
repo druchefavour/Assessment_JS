4c2ebf5615405e01b095444fa165b12bdf50554b
druchefavour <druchefavour@gmail.com>
2017-06-03 12:12:03 -0500

    commit function test

--- function.js
+++ function.js
@@ -33,21 +33,44 @@ exports.functionsAnswers = {
 
   partial: function(fn, str1, str2) {
 
+     return function(str3) {
+        return fn(str1, str2, str3);
+      }
   },
 
   useArguments: function() {
 
+     return Array.prototype.slice.call(arguments).reduce(function(a, b) {
+        return a + b;
+      });
   },
 
   callIt: function(fn) {
 
+    var arguments = Array.prototype.slice.call(arguments);
+      var fn = arguments[0];
+      arguments.shift();
+      return fn.apply(this, arguments);
   },
 
   partialUsingArguments: function(fn) {
 
-  },
+    var args1 = Array.prototype.slice.call(arguments);
+    var fn = args1[0];
+    args1.shift();
+    return function() {
+      var args2 = Array.prototype.slice.call(arguments);
+      return fn.apply(this, args1.concat(args2));
+    };
+},
 
-  curryIt: function(fn) {
-
+curryIt: function(fn) {
+  var args1 = Array.prototype.slice.call(arguments);
+  var fn = args1[0];
+  args1.shift();
+  return function() {
+    var args2 = Array.prototype.slice.call(arguments);
+    return fn.apply(this, args1.concat(args2));
+    };
   }
-};
+ };
